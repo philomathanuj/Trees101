98434eed263297a77c4b4bff6604158cb81728a8
Anuj Sharma <philomath.anuj@gmail.com>
2021-07-04 17:15:22 +0530

    Check if two nodes are cousins

---
+++ src/main/java/com/philomath/trees/questions/CheckForCousinNodes.java
@@ -0,0 +1,85 @@
+package com.philomath.trees.questions;
+
+public class CheckForCousinNodes {
+    TreeNode root;
+
+    TreeNode node4 = new TreeNode(4);
+    TreeNode node6 = new TreeNode(6);
+    TreeNode node5 = new TreeNode(5);
+
+    class TreeNode{
+        int data;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode(int data){
+            this.data = data;
+        }
+        public TreeNode(int data, TreeNode left, TreeNode right){
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public void createTree(){
+        this.root = new TreeNode(1,
+                new TreeNode(2,
+                        node4, node5),
+                new TreeNode(3, node6, new TreeNode(7)));
+    }
+
+    public boolean isCousin(TreeNode node1, TreeNode node2){
+        if(node1 == node2){
+            return false;
+        }
+        if(!isSibling(node1, node2) && getLevel(node1) == getLevel(node2)){
+            return true;
+        }
+        return false;
+    }
+
+    private int getLevel(TreeNode node) {
+        return getLevel(root, node,0);
+    }
+
+    private int getLevel(TreeNode root, TreeNode node, int level) {
+        if(root == node || root == null){
+            return level;
+        }
+        if(root.left != null){
+            level = getLevel(root.left, node, level+1);
+        }
+        if(level != 0){
+            return level;
+        }
+        else{
+            level = getLevel(root.right, node, level+1);
+        }
+        return level;
+
+    }
+
+
+    private boolean isSibling(TreeNode node1, TreeNode node2) {
+        return isSibling(node1, node2, this.root);
+    }
+
+    private boolean isSibling(TreeNode node1, TreeNode node2, TreeNode root) {
+        if(root == null) return false;
+        if((node1 == root.left && node2 == root.right) ||
+                (node1 == root.right && node2 == root.left)
+            || isSibling(node1,node2, root.left)
+            || isSibling(node1, node2, root.right)){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        CheckForCousinNodes c = new CheckForCousinNodes();
+        c.createTree();
+        System.out.println(c.isCousin(c.node4, c.node5));
+        System.out.println(c.isCousin(c.node5, c.node6));
+    }
+
+}
