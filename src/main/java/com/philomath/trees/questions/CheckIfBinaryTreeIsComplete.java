141894ec12ee2606baa44889259115240e72e6d8
Anuj Sharma <philomath.anuj@gmail.com>
2021-07-04 16:43:09 +0530

    Check if binary tree is complete binary tree

--- src/main/java/com/philomath/trees/questions/CheckIfBinaryTreeIsComplete.java
+++ src/main/java/com/philomath/trees/questions/CheckIfBinaryTreeIsComplete.java
@@ -3,6 +3,19 @@ package com.philomath.trees.questions;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * The idea to solve this problem is simple. We do a usual level order traversal and
+ * whenever we see that there is a node which does not have both left and right child,
+ * then we set nonFullNodeSeen = true. This indicates that while doing level order traversal,
+ * we have seen our first node with either right or left or both children absent.
+ * And for a complete binary tree, all the nodes visited during level order traversal
+ * after visiting a nonFullNode would be leaf nodes. If this is not true, we return false.
+ * For example, in the left-hand side complete tree shown above, once we visit node '3' which is a
+ * nonFullNode, the nodes visited after this node are node '4','5' and '6' all of which are leaf nodes.
+ *
+ * We also need to check if there is any node with right child but with no left child. In this case, we simply return false.
+ * https://www.ideserve.co.in/learn/check-whether-binary-tree-is-complete-tree-or-not
+ */
 public class CheckIfBinaryTreeIsComplete {
     TreeNode root;
 
